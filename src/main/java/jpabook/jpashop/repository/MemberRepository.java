6b66974ca68d3192f944fe4d1205be3c32d36ec1
ksj <rlatpwns54@naver.com>
2022-03-16 21:13:01 +0900

    회원서비스개발-Repository

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        //createQuery(JPQL넣으면 됨, 반환 타입)
+        //JPQL은 SQL이랑 조금씩 차이가 있음 : SQL은 테이블을 대상으로 쿼리, JPQL은 Entity 객체를 대상으로 쿼리
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
